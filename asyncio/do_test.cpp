f5116019d4077f9e5b3f4995dffde6cd19ca4dba
zhangli <zhangli@joypace.com>
2017-08-08 17:44:51 +0800

    add TestEventLogger

--- asyncio/do_test.cpp
+++ asyncio/do_test.cpp
@@ -3,6 +3,42 @@
 #include <cstdlib>
 #include <spdlog/spdlog.h>
 
+#define ENABLE_ASYNCIO_LOG
+#include "log.hpp"
+using namespace Catch;
+struct LogEvents : TestEventListenerBase {
+
+  using TestEventListenerBase::TestEventListenerBase; // inherit constructor
+  // The whole test run, starting and ending
+  // void testRunStarting(TestRunInfo const &testRunInfo) {}
+  // void testRunEnded(TestRunStats const &testRunStats) {}
+
+  // Test cases starting and ending
+  void testCaseStarting(TestCaseInfo const &testInfo) {
+    LOG_INFO("Case start: {}", testInfo.name);
+  }
+  void testCaseEnded(TestCaseStats const &testCaseStats) {
+    LOG_INFO("Case ended: {}", testCaseStats.testInfo.name);
+  }
+
+  // Sections starting and ending
+  void sectionStarting(SectionInfo const &sectionInfo) {
+    LOG_INFO("Section start: {}", sectionInfo.name);
+  }
+  void sectionEnded(SectionStats const &sectionStats) {
+    LOG_INFO("Section ended: {}", sectionStats.sectionInfo.name);
+  }
+
+  // // Assertions before/ after
+  // void assertionStarting(AssertionInfo const &assertionInfo) {}
+  // bool assertionEnded(AssertionStats const &assertionStats) {}
+
+  // A test is being skipped (because it is "hidden")
+  // void skipTest(TestCaseInfo const &testInfo) {}
+};
+
+CATCH_REGISTER_LISTENER(LogEvents)
+
 int main(int argc, char *argv[]) {
   // global setup...
   if (const char *env_p = std::getenv("LOG_DEBUG")) {
@@ -11,7 +47,7 @@ int main(int argc, char *argv[]) {
     spdlog::set_level(spdlog::level::info);
   }
 
-  int result = Catch::Session().run(argc, argv);
+  int result = Session().run(argc, argv);
 
   // global clean-up...
 
